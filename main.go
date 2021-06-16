051c99e2c188b53f42576b52bfb0bf0a64347242
altair <17752254783@163.com>
2021-06-16 16:39:51 +0800

    persistence to disk

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unsafe"
 )
 
 const PROMPT = ">>> "
@@ -18,7 +19,7 @@ const (
 
 const (
 	MAX_ID_LEN         = 8
-	MAX_NAME_LEN       = 64
+	MAX_NAME_LEN       = 56
 	MAX_EMAIL_LEN      = 64
 	MAX_PAGE_PER_TABLE = 1024
 	PAGE_SZ            = 4096
@@ -38,9 +39,14 @@ func (r Row) String() string {
 	return fmt.Sprintf("%v %v %v", r.id, string(r.name[:]), string(r.email[:]))
 }
 
+type Pager struct {
+	fd    *os.File
+	pages [MAX_PAGE_PER_TABLE][]Row
+}
+
 type Table struct {
 	num_rows uint32
-	pages    [MAX_PAGE_PER_TABLE][]Row
+	pager    *Pager
 }
 
 type Statement struct {
@@ -54,20 +60,64 @@ func row_slot(t *Table, row_num uint) (*Row, error) {
 		return nil, errors.New("table full")
 	}
 
-	page := t.pages[row_num/MAX_ROW_PER_PAGE]
+	page := t.pager.pages[row_num/MAX_ROW_PER_PAGE]
 	if page == nil {
-		t.pages[row_num/MAX_ROW_PER_PAGE] = make([]Row, MAX_ROW_PER_PAGE)
-		page = t.pages[row_num/MAX_ROW_PER_PAGE]
+		t.pager.pages[row_num/MAX_ROW_PER_PAGE] = make([]Row, MAX_ROW_PER_PAGE)
+		page = t.pager.pages[row_num/MAX_ROW_PER_PAGE]
 	}
 	return &page[row_num%MAX_ROW_PER_PAGE], nil
 }
 
-func do_meta_command(op string) {
+func to_byte(p unsafe.Pointer, n int) []byte {
+	return (*[PAGE_SZ]byte)(p)[:n]
+}
+
+func (p *Pager) get_page(page_num uint) ([]Row, error) {
+	if page_num > MAX_PAGE_PER_TABLE {
+		return nil, errors.New("requested page exceeds max page")
+	}
+	page := p.pages[page_num]
+	if page == nil {
+		p.pages[page_num] = make([]Row, MAX_ROW_PER_PAGE)
+		file_len, err := p.fd.Stat()
+		if err != nil {
+			panic(err)
+		}
+		cur_page := file_len.Size() / PAGE_SZ
+		if int64(page_num) < cur_page {
+			n, err := p.fd.ReadAt(to_byte(unsafe.Pointer(&page), 4096), int64(page_num*PAGE_SZ))
+			if err != nil || n != PAGE_SZ {
+				panic(err)
+			}
+		}
+		page = p.pages[page_num]
+	}
+	return page, nil
+}
+
+func (p *Pager) flush_page(page_num int) {
+	if page_num > MAX_PAGE_PER_TABLE || p.pages[page_num] == nil {
+		return
+	}
+	page := p.pages[page_num]
+	n, err := p.fd.WriteAt(to_byte(unsafe.Pointer(&page[0]), 4096), int64(page_num*PAGE_SZ))
+	// fmt.Printf("%v %v %v\n", page[0].id, page[0].email, page[0].name)
+	if err != nil {
+		panic(err)
+	}
+	if n != PAGE_SZ {
+		panic("partially flush a page")
+	}
+	p.pages[page_num] = nil
+}
+
+func do_meta_command(T *Table, op string) {
 	if op[0] != '.' {
 		panic("meta operation should start with \".\"")
 	}
 	switch op {
 	case ".exit":
+		close_DB(T)
 		fmt.Println("Database closed")
 		os.Exit(1)
 	default:
@@ -118,19 +168,21 @@ handle_err:
 }
 
 func execute_statement(t *Table, smt Statement) {
-	switch smt.smt_type {
-	case INSERT_STATEMENT:
+	insert_func := func() {
 		if t.num_rows > MAX_ROW_PER_TABLE {
 			panic("table full")
 		}
 		cur_row, err := row_slot(t, uint(t.num_rows))
 		if err != nil {
-			// fmt.Println(err)
+			fmt.Println(err)
 			return
 		}
 		row_copy(cur_row, &smt.row)
 		t.num_rows++
-	case SELECT_STATEMENT:
+	}
+
+	select_func := func() {
+		fmt.Println()
 		for i := 0; i < int(t.num_rows); i++ {
 			row, err := row_slot(t, uint(i))
 			if err != nil {
@@ -139,28 +191,71 @@ func execute_statement(t *Table, smt Statement) {
 			}
 			fmt.Println(*row)
 		}
+		fmt.Print(PROMPT)
+	}
+
+	switch smt.smt_type {
+	case INSERT_STATEMENT:
+		insert_func()
+	case SELECT_STATEMENT:
+		select_func()
 	default:
 		panic("unknown statement")
 	}
 }
 
+func open_DB(file string) *Table {
+	fd, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE, 0o666)
+	if err != nil {
+		panic(err)
+	}
+	pager := Pager{fd: fd}
+	length, err := fd.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if length.Size()%PAGE_SZ != 0 {
+		panic("databse file error")
+	}
+	db := Table{uint32(length.Size()) / uint32(unsafe.Sizeof(Row{})), &pager}
+	return &db
+}
+
+func close_DB(t *Table) {
+	full_pages := t.num_rows / MAX_ROW_PER_PAGE
+	if t.num_rows%MAX_ROW_PER_PAGE != 0 {
+		full_pages++
+	}
+	fmt.Println(full_pages, " pages to flush", t.num_rows)
+	for i := 0; i < int(full_pages); i++ {
+		t.pager.flush_page(i)
+	}
+	err := t.pager.fd.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handle_request(input string, T *Table) {
+	if input[0] == '.' {
+		do_meta_command(T, input)
+	} else {
+		smt, err := prepare_statement(input)
+		if err != nil {
+			return
+		}
+		execute_statement(T, smt)
+	}
+}
+
 func main() {
 	scan := bufio.NewScanner(os.Stdin)
-	T := Table{}
-	// fmt.Println(unsafe.Sizeof(Row{}), MAX_ROW_PER_PAGE)
+	T := open_DB("stu.db")
+	fmt.Println(unsafe.Sizeof(Row{}), MAX_ROW_PER_PAGE)
 	for {
 		fmt.Print(PROMPT)
 		scan.Scan()
 		input := scan.Text()
-		if input[0] == '.' {
-			do_meta_command(input)
-		} else {
-			smt, err := prepare_statement(input)
-			fmt.Println(smt.row)
-			if err != nil {
-				continue
-			}
-			execute_statement(&T, smt)
-		}
+		go handle_request(input, T)
 	}
 }
